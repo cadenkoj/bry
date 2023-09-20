97f99bd07f4182220e20409d1a6d539ca52936b0
Ubuntu <ubuntu@ip-172-31-87-216.ec2.internal>
2023-09-20 01:37:43 +0000

    fix autocomplete

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'bry',
+            script: 'main.py',
+            interpreter: 'python3',
+            instances: 1,
+            out_file: 'logs/out.log',
+            error_file: 'logs/err.log'
+        }
+    ]
+};
