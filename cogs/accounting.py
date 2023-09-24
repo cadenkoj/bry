94f5ea2545cf9ec511b788e5d9b50c279d25b410
ehxpulse <ehxpulse>
2023-09-24 02:32:07 -0400

    quick change

--- cogs/accounting.py
+++ cogs/accounting.py
@@ -97,10 +97,12 @@ class Accounting(commands.Cog):
 
         log_embed = discord.Embed(
             color=0x77ABFC,
-            description=f"{customer.mention} (`{customer.id}`) has bought **{name}** for **${display_price}**.",
+            description=f"{customer.mention} (`{customer.id}`) purchased **{name}** for **${display_price}**.",
         )
 
-        log_embed.set_author(name=customer, icon_url=customer.display_avatar.url)
+        log_embed.set_author(
+            name=customer, icon_url=f"{customer.display_avatar.url} ({customer.id})"
+        )
         log_embed.add_field(name=f"__Username__", value=username, inline=True)
         log_embed.add_field(name=f"__{payment.name}__", value=info, inline=True)
 
