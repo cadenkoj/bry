5c71bc83c4002c0da925a7a1a525b96079cb466f
Ubuntu <ubuntu@ip-172-31-87-216.ec2.internal>
2023-09-27 02:11:08 +0000

    eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'bry',
             script: 'main.py',
-            interpreter: 'python3',
+            interpreter: 'python3.11',
             instances: 1,
             out_file: 'logs/out.log',
             error_file: 'logs/err.log'
