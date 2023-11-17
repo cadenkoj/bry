2f9d80f129d5e9206d9862e209e84e1daa936664
Caden <cldoc507@gmail.com>
2023-11-17 11:04:02 -0500

    me doing a stupid

--- cogs/info.py
+++ cogs/info.py
@@ -9,11 +9,11 @@ class Info(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.hybrid()
+    @commands.hybrid_command()
     async def pn(self, ctx: commands.Context):
         await ctx.send(f"This is for digital goods. I have already received what I paid for. I will not chargeback under any circumstance. I am fully aware that making any attempt to chargeback this payment is considered fraud and against the seller's policy.")
 
-    @commands.hybrid()
+    @commands.hybrid_command()
     async def ps(self, ctx: commands.Context):
         is_staff = self.bot.config.roles.staff in ctx.author.roles
         if not is_staff:
@@ -21,19 +21,19 @@ class Info(commands.Cog):
 
         await ctx.send("https://www.roblox.com/games/2788229376/1M-CODEs-Da-Hood?privateServerLinkCode=83706208461702631056500770002026")
 
-    @commands.hybrid()
+    @commands.hybrid_command()
     async def ltc(self, ctx: commands.Context):
         await ctx.send("```LTX2cZ9DoFR2gErZdCaaLA1ovFzdNsfTmH```")
 
-    @commands.hybrid()
+    @commands.hybrid_command()
     async def btc(self, ctx: commands.Context):
         await ctx.send("```bc1qcza9p80drr8wzvdpn0vzegpsglmkvgsmmukuxe```")
 
-    @commands.hybrid()
+    @commands.hybrid_command()
     async def eth(self, ctx: commands.Context):
         await ctx.send("```0x7462F240169b7411fDC83C607e5C13fbcBC7E988```")
 
-    @commands.hybrid()
+    @commands.hybrid_command()
     async def paypal(self, ctx: commands.Context, amount: int):
         is_staff = self.bot.config.roles.staff in ctx.author.roles
         if not is_staff:
