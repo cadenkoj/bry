e5ff80efaf5635140a17fb2af62fe4cc2ca4f429
Caden <cldoc507@gmail.com>
2024-03-11 18:12:45 -0400

    fix another multiline f-string

--- utils.py
+++ utils.py
@@ -192,8 +192,7 @@ class LogFormatter(logging.Formatter):
 
     FORMATS = {
         level: logging.Formatter(
-            f"\x1b[30;1m%(asctime)s\x1b[0m {
-                colour}%(levelname)-8s\x1b[0m \x1b[35m%(name)s\x1b[0m %(message)s",
+            f"\x1b[30;1m%(asctime)s\x1b[0m {colour}%(levelname)-8s\x1b[0m \x1b[35m%(name)s\x1b[0m %(message)s",
             "%Y-%m-%d %H:%M:%S",
         )
         for level, colour in LEVEL_COLOURS
